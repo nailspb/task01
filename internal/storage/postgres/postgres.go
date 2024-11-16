3653ed95ffcb6cb1ff4036bf5b0de4404e0033a0
GVE <viktor.glebov@ya.ru>
2024-11-16 19:52:17 +0300

    Ðadded database storage, code refactoring

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -1,1 +1,50 @@
 package postgres
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"task01/internal/models"
+)
+
+type Storage struct {
+	db *gorm.DB
+}
+
+func New() *Storage {
+	connection := "host=srv2.spartatn.ru user=user password=super_puper_user_password dbname=tests port=5430 sslmode=disable"
+	DB, err := gorm.Open(postgres.Open(connection), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+	return &Storage{
+		db: DB,
+	}
+}
+
+func (s *Storage) Migrate(dst ...any) error {
+	for _, d := range dst {
+		err := s.db.AutoMigrate(&d)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *Storage) AddTask(task models.Message) error {
+	res := s.db.Create(&task)
+	if res.Error != nil {
+		return res.Error
+	}
+	return nil
+}
+
+func (s *Storage) GetTasks() ([]models.Message, error) {
+	messages := make([]models.Message, 0)
+	res := s.db.Find(&messages)
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return messages, nil
+}
--- main.go
+++ main.go
@@ -2,50 +2,63 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
-	"strconv"
+	"task01/internal/models"
+	"task01/internal/storage/postgres"
 )
 
-var task int
-
-func GetTaskHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte(fmt.Sprintf("Hello, %d", task)))
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
+type Storage interface {
+	GetTasks() ([]models.Message, error)
+	AddTask(task models.Message) error
 }
 
-type PostTaskHandlerRequest struct {
-	Task string `json:"task"`
+func GetTaskHandler(storage Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tasks, err := storage.GetTasks()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		serialized, err := json.Marshal(tasks)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		_, err = w.Write(serialized)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}
 }
 
-func PostTaskHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := io.ReadAll(r.Body)
-	if err != nil || len(body) < 3 {
-		w.WriteHeader(http.StatusBadRequest)
-		return
+func PostTaskHandler(storage Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		dec := json.NewDecoder(r.Body)
+		var task models.Message
+		err := dec.Decode(&task)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if err = storage.AddTask(task); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		w.WriteHeader(http.StatusOK)
 	}
-	var req PostTaskHandlerRequest
-	if err = json.Unmarshal(body, &req); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	newTask, err := strconv.Atoi(req.Task)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-	}
-	task = newTask
-	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
+	str := postgres.New()
+	err := str.Migrate(&models.Message{})
+	if err != nil {
+		panic(err)
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle("GET /task", http.HandlerFunc(GetTaskHandler))
-	mux.Handle("POST /task", http.HandlerFunc(PostTaskHandler))
+	mux.Handle("GET /task", GetTaskHandler(str))
+	mux.Handle("POST /task", PostTaskHandler(str))
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		panic(err)
 	}
